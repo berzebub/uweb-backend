ae02bdeb3a1ad1742c378285b41a4d43fa2c642c
art <pakkawat.kh@gmail.com>
2021-04-18 02:35:45 +0700

    make countrybrief

--- src/pages/countrybrief.vue
+++ src/pages/countrybrief.vue
@@ -1,26 +1,29 @@
 <template>
   <q-page>
     <div class="col q-py-lg" align="center">
-      <q-card class="my-card q-mx-lg" style="width: 500px;">
+      <q-card class="my-card q-mx-lg" style="width: 900px;">
+        <div class="text-h5 q-pt-md">Country Brief Data Generator</div>
         <div class="q-pa-md">
           <q-select
             outlined
             v-model="dataInput.year"
             :options="year"
             label="Year"
-            style="width: 300px;"
+            dense
+            style="width: 600px;"
           />
         </div>
 
-        <div class="q-pa-md">
+        <div>
           <q-select
             outlined
             v-model="dataInput.country"
             :options="countryList"
+            dense
             emit-value
             map-options
             label="Economy"
-            style="width: 300px;"
+            style="width: 600px;"
           />
         </div>
 
@@ -29,13 +32,14 @@
             outlined
             v-model="dataInput.dataSet"
             :options="dataSet"
-            label="Data set"
-            style="width: 300px;"
+            dense
+            emit-value
+            map-options
+            label="Action"
+            style="width: 600px;"
           />
         </div>
-        <div class="q-pb-md">
-          <u>Explaination of Data set</u>
-        </div>
+
         <div class="q-pb-md">
           <q-btn no-caps class="bg4 font-content" label="Genereate Data" @click="genData()" />
         </div>
@@ -50,10 +54,37 @@ export default {
   data() {
     return {
       year: [2007, 2011],
-      dataSet: ["set#1", "set#2"],
+      dataSet: [
+        {
+          label: "Step 1 : Reset all data",
+          value: 1,
+        },
+        {
+          label:
+            "Step 2 : Recalculate Value-added structure of grossexports to world by exporter (all economies)",
+          value: 2,
+        },
+        {
+          label:
+            "Step 3 : Value-added structure of grossexports to world by exporter",
+          value: 3,
+        },
+        {
+          label: "Step 4 : Gross and value-added trade balance",
+          value: 4,
+        },
+        {
+          label: "Step 5 : Global value chain relationships",
+          value: 5,
+        },
+        {
+          label: "Step 6 : Participation in GVCs",
+          value: 6,
+        },
+      ],
       dataInput: {
         year: 2007,
-        dataSet: "set#1",
+        dataSet: 1,
         country: "ARG",
       },
       countryList: [
@@ -92,8 +123,53 @@ export default {
         this.countryList.push(temp);
       });
     },
-    genData() {
-      console.log(this.dataInput);
+    async genData() {
+      this.loadingShow();
+      //Step 1
+      if (this.dataInput.dataSet == 1) {
+        let url = this.serverPath + "u_api/cbreset.php";
+        let res = await axios.post(url, JSON.stringify(this.dataInput));
+        this.loadingHide();
+        this.notifyGreen("Action completely");
+      } else if (this.dataInput.dataSet == 2) {
+        let url = this.serverPath + "u_api/cbstep2.php";
+        let res = await axios.post(url, JSON.stringify(this.dataInput));
+        this.loadingHide();
+        this.notifyGreen("Action completely");
+      }
+      //step 3
+      else if (this.dataInput.dataSet == 3) {
+        let url = this.serverPath + "u_api/cbstep3.php";
+
+        let res = await axios.post(url, JSON.stringify(this.dataInput));
+        this.loadingHide();
+        if (res.data != "finish") {
+          this.notifyRed("Please choose action step 2 first.");
+        } else {
+          this.notifyGreen("Action completely");
+        }
+      }
+      //step 4
+      else if (this.dataInput.dataSet == 4) {
+        let url = this.serverPath + "u_api/cbstep4.php";
+        let res = await axios.post(url, JSON.stringify(this.dataInput));
+        this.loadingHide();
+        this.notifyGreen("Action completely");
+      }
+      //step 5
+      else if (this.dataInput.dataSet == 5) {
+        let url = this.serverPath + "u_api/cbstep5.php";
+        let res = await axios.post(url, JSON.stringify(this.dataInput));
+        this.loadingHide();
+        this.notifyGreen("Action completely");
+      }
+      //step 6
+      else if (this.dataInput.dataSet == 6) {
+        let url = this.serverPath + "u_api/cbstep6.php";
+        let res = await axios.post(url, JSON.stringify(this.dataInput));
+        this.loadingHide();
+        this.notifyGreen("Action completely");
+      }
     },
     setDataTemp(data) {
       this.year = [];
@@ -113,6 +189,8 @@ export default {
   },
   mounted() {
     this.loadData();
+    this.dataInput.year = 2017;
+    this.dataInput.country = "THA";
   },
 };
 </script>
